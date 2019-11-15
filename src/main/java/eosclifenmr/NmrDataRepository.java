036e8ce55377f1acb8443390bbd73788889bdcb7
Andrea <andreagia71@gmail.com>
2019-11-15 15:11:50 +0100

    Added Nmrdata

--- src/main/java/eosclifenmr/NmrDataRepository.java
+++ src/main/java/eosclifenmr/NmrDataRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 interface   NmrDataRepository extends JpaRepository<NmrData, Long> {
-    List<NmrData> findByIdligand(String idlands);
+    List<NmrData> findByIdligand(String idligand);
 }
